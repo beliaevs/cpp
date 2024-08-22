9623b7606048e61f46f14a94b11fb342ae8cc4a7
Alexander Beliaev <beliaevs@gmail.com>
2024-08-23 02:34:22 +0300

    extra trace output

--- noexcept_move/main.cpp
+++ noexcept_move/main.cpp
@@ -8,15 +8,37 @@ int main()
 		std::vector<Movable> v;
 		v.reserve(3);
 		for (auto n : args)
-			v.emplace_back(n);
-		v.emplace_back(4);
+			v.emplace_back(n); // elements are constructed in-place
+		std::cout << "extra emplace_back\n";
+		v.emplace_back(4); // new buffer is allocated
+		// new element is constructed in-place
+		// other elements are moved to new buffer
+		// moved-from buffer is destroyed
+		std::cout << "extra push_back\n";
+		v.push_back(Movable(5)); // new temporary element is created
+		// new buffer is allocated
+		// temporary is moved into new buffer
+		// other elements are moved into new buffer
+		// moved-from buffer is destroyed
+		// vector is destroyed
 	}
 	{
 		std::vector<NonMovable> v;
 		v.reserve(3);
 		for (auto n : args)
-			v.emplace_back(n);
-		v.emplace_back(4);
+			v.emplace_back(n); // elements are constructed in-place
+		std::cout << "extra emplace_back\n";
+		v.emplace_back(4); // new buffer is allocated
+		// new element is constructed in-place
+		// other elements are *copied* to new buffer
+		// old buffer is destroyed
+		std::cout << "extra push_back\n";
+		v.push_back(NonMovable(5)); // temporary element is created
+		// new buffer is allocated
+		// temporary is moved into new buffer
+		// other elements are copied into new buffer
+		// old buffer is destroyed
+		// vector is destroyed
 	}
 	return 0;
 }
