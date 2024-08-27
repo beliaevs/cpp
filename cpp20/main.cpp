7fce381fd8f059e6420428b7a26558a4e423a547
Alexander Beliaev <beliaevs@gmail.com>
2024-08-27 21:46:45 +0300

    operator <=>

--- cpp20/main.cpp
+++ cpp20/main.cpp
@@ -1,7 +1,24 @@
 #include<iostream>
 
+// spaceship operator
+struct Comp
+{
+	int a;
+	double b;
+	char c;
+	constexpr std::partial_ordering operator<=>(const Comp& rhs) const = default;
+};
+
+
+
 int main()
 {
 	std::cout << "cpp20 features test\n";
+	Comp c1{ 1, 0., 'a' };
+	Comp c2{ 1, 0., 'b' };
+
+	std::cout << (c1 < c2) << ", " << (c1 > c2) << ", " << (c1 == c2) << "\n";
+
+
 	return 0;
 }
