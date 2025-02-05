244c2584c170086e2755ce50d450642c8ccc3345
Alexander Beliaev <beliaevs@gmail.com>
2025-02-05 15:28:04 +0400

    Cyrillic IO for linux

---
+++ russian/main.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+#include<string>
+#include<locale>
+
+int main()
+{
+  // cyrillic input-outout for linux
+    std::locale loc("");
+    std::locale::global(loc);
+    std::wcout << L"Введите строку: " << std::endl;
+    std::wstring ws;
+    std::wcin >> ws;
+    std::wcout << ws << L"\n";
+  return 0;
+}
