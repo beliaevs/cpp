6f6d6e5f57f7648cc47be2649cca14e1c8d6df1f
Alexander Beliaev <beliaevs@gmail.com>
2025-02-05 15:40:48 +0300

    cyrillic IO on windows

---
+++ russian/mainwin.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+#include<string>
+#include<io.h> // for _setmode
+#include<fcntl.h> // for _O_UI16TEXT
+
+void set_utf16_mode()
+{
+    _setmode(_fileno(stdout), _O_U16TEXT);
+    _setmode(_fileno(stdin), _O_U16TEXT);
+    _setmode(_fileno(stderr), _O_U16TEXT);
+}
+
+int main()
+{
+    set_utf16_mode();
+
+    std::wcout << L"Введите строку:" << std::endl;
+    std::wstring ws;
+    std::wcin >> ws;
+    std::wcout << ws << L"\n";
+    return 0;
+}
